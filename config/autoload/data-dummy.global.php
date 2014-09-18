e7801c8c543565c4706ef8b3884d46237155b1fe
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2014-09-18 09:13:35 +0100

    Dummy data urls added to config

---
+++ config/autoload/data-dummy.global.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+    'data' => array(
+        'dummy' => array(
+            'accounts' => array(
+                'https://raw.githubusercontent.com/TransformCore/elasticsearch-example-docs/master/data/account-1.json',
+                'https://raw.githubusercontent.com/TransformCore/elasticsearch-example-docs/master/data/account-2.json',
+                'https://raw.githubusercontent.com/TransformCore/elasticsearch-example-docs/master/data/account-3.json',
+                'https://raw.githubusercontent.com/TransformCore/elasticsearch-example-docs/master/data/account-4.json',
+                'https://raw.githubusercontent.com/TransformCore/elasticsearch-example-docs/master/data/account-5.json',
+            )
+        )
+    )
+);
